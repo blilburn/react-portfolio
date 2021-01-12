351191fd6f709d3e94d81a278a3a4a1633b2d08a
Bryan Lilburn <bryanlilburn@gmail.com>
2021-01-11 19:10:32 -0800

    Added About page. Added row and column components

---
+++ portfolio/src/pages/About.js
@@ -0,0 +1,40 @@
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+function About() {
+    return (
+        <div>
+            <Container style={{ marginTop: 30 }}>
+                <Row>
+                    <Col size="md-12">
+                        {/* <Hero></Hero> */}
+                    </Col>
+                    <Col size="md-12">
+                        <h1>I'm Bryan Lilburn</h1>
+                        <p> Web Developer and Programmer</p>
+        
+                        <table class="table table-responsive">
+                    <tr>
+                        <td>Linked In</td>
+                        <td>:</td>
+                        {/* <td><a href="https://www.linkedin.com/in/bryan-lilburn-32ba181b8" target="_blank">Link to my profile</a></td> */}
+                   </tr>
+                    <tr>
+                        <td>Github</td>
+                        <td>:</td>
+                        {/* <td><a href="https://github.com/blilburn" target="_blank">Link to my page</a></td> */}
+                    </tr>
+                </table>
+                        <p>                I currently work in the health care industry and am improving my skills in web development and
+                programming. I have always been fascinated with web development as it allows me to focus on creative
+                problem-solving. Each project is a new challenge that provides an opportunity to learn and innovate. I
+                am currently working on a few web development projects so please return so that you can see my future
+                projects.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default About;
